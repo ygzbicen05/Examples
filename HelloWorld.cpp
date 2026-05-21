5677324e7d5b862a3628b8ccd9ca64f37bd34de1
Yağız Ali BİÇEN <ygzbicen05@gmail.com>
2026-05-21 11:39:03 +0300

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	printf("Hello, world!");
+	return 0;
+}
+*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+	cout<<"hello, c++"<<endl;
+	return 0;
+}
